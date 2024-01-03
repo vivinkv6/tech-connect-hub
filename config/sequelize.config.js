4c8f4ad29d5fa3ecc85a113881fa18d89f0d2679
vivinkv6 <vivinkv0@gmail.com>
2024-01-03 11:45:27 +0530

    update

--- config/sequelize.config.js
+++ config/sequelize.config.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 
 const sequelizeConfig = new Sequelize({
+  port:process.env.DBPORT,
   username: process.env.USERNAME,
   password: process.env.PASSWORD,
   database: process.env.DATABASE,
