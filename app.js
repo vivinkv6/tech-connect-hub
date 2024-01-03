81a948032b1ffefa5989384727e3027ad93df366
vivinkv6 <vivinkv0@gmail.com>
2024-01-03 12:11:26 +0530

    update

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ const sequelizeConfig = require("./config/sequelize.config");
 var app = express();
 
 // view engine setup
+app.set('views', '/opt/render/project/src/views');
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
