9854f9b6d928ff33654075590d98ec43b825653b
vivinkv6 <vivinkv0@gmail.com>
2024-04-02 15:27:34 +0530

    update

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,20 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-
+const jwt = require("jsonwebtoken");
+const userModel = require("../models/user/loginModel");
+require("dotenv").config();
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index');
+router.get("/", async function (req, res, next) {
+  if (req?.cookies?.user) {
+    const id = jwt.verify(req.cookies.user, process.env.JWT_SECRET_TOKEN);
+    const findUser = await userModel.findByPk(id);
+    if (findUser) {
+      console.log(`Authenticated User: ${findUser.dataValues.name}`);
+    }
+  } else {
+    console.log("UnAuthenticated User");
+  }
+  res.render("index");
 });
 
 module.exports = router;
